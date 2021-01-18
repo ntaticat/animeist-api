72a5d207c408d57aa893f135894daeb417e82d61
Rafael Estrada <restradap1600@gmail.com>
2021-01-18 15:11:48 -0600

    Se desarrolló los metodos GET de la API

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,42 @@ const app = express();
 
 const Anime = require("../models/anime");
 
+app.get("/anime/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    Anime.findById(id, (err, dbAnime) => {
+        if(err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            dbAnime
+        });
+    });
+});
+
+app.get("/animes/:estado", (req, res) => {
+    const estado = Number(req.params.estado);
+
+    Anime.find({estado}).exec((err, animes) => {
+        if(err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            animes
+        });
+    });
+});
+
 app.post("/anime", (req, res) => {
     
     const body = req.body;

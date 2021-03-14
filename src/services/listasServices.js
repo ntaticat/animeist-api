ed6ae49a932ccc8362442336799e9dd4bed4c592
Rafael Estrada <restradap1600@gmail.com>
2021-03-14 16:12:12 -0600

    update listasServices file

--- src/services/listasServices.js
+++ src/services/listasServices.js
@@ -1,7 +1,45 @@
 const listasModel = require('../models/listasModel');
 
-exports.obtenerMensaje = () => {
-    return {
-        msg: "Hola Mundo"
+exports.createLista = async (usuarioId) => {
+    try {
+        const newLista = new listasModel({
+            usuario: usuarioId,
+            animes: []
+        });
+        return await newLista.save();
+    } catch (error) {
+        throw error;
+    }
+};
+
+exports.addAnimeLista = async (listaId, anime, estado) => {
+    try {
+        const lista = await listasModel.findById(listaId).exec();
+        await lista.animes.push({
+            nombre: anime,
+            estado
+        });
+        return await lista.save();
+    } catch (error) {
+        throw error;
+    }
+};
+
+exports.removeAnimeLista = async (listaId, nombre) => {
+    try {
+        const lista = await listasModel.findById(listaId).exec();
+        lista.animes.push({
+            nombre: anime,
+            estado
+        });
+        const indiceAnime = lista.animes.reduce((anime, index) => {
+            if (anime.nombre == nombre) {
+                return index;
+            }
+        });
+        lista.animes.splice(indiceAnime, 1);
+        return await lista.save();
+    } catch (error) {
+        throw error;
     }
 };

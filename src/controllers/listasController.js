034a68b1ed891a726cdb2c91af0e93164e711364
Rafael Estrada <restradap1600@gmail.com>
2021-03-14 16:13:01 -0600

    update listasController file

--- src/controllers/listasController.js
+++ src/controllers/listasController.js
@@ -1,7 +1,30 @@
 const { request, response } = require('express');
 const listasServices = require('../services/listasServices');
 
-exports.postListaAnime = async (req = request, res = response) => {
-    const mensaje = listasServices.obtenerMensaje();
-    res.json(mensaje);
+exports.postAnime = async (req = request, res = response) => {
+    try {
+        const { listaId, anime, estado } = req.body.datos;
+        const dbLista = await listasServices.addAnimeLista(listaId, anime, estado);
+        res.status(200).json({
+            lista: dbLista
+        });
+    } catch (error) {
+        res.status(400).json({
+            error
+        });
+    }
+};
+
+exports.deleteAnime = async (req = request, res = response) => {
+    try {
+        const { listaId, nombre } = req.body.datos;
+        const dbLista = await listasServices.removeAnimeLista(listaId, nombre);
+        res.status(200).json({
+            lista: dbLista
+        });
+    } catch (error) {
+        res.status(400).json({
+            error
+        });
+    }
 };
